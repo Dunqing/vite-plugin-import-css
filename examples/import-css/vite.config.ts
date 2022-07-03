253c722426f815b4ba7386589c941870f46bdf57
dengqing <dengqing0821@gmail.com>
2022-07-03 10:16:56 +0800

    chore: add examples

---
+++ examples/import-css/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import importCss from "vite-plugin-import-css"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'cjs' ? 'js' : 'mjs'}`,
+    },
+    emptyOutDir: false,
+    rollupOptions: {
+      external: ['vite']
+    },
+  },
+  plugins: [importCss()]
+})
